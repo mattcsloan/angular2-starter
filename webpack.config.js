523c90179c25aa50899e94f98defd40ee18fe577
ecozoic <johnnn.mitchell@gmail.com>
2016-04-02 21:42:05 -0400

    add sass lint

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SassLintPlugin = require('sasslint-webpack-plugin');
 
 const basePlugins = [
   new webpack.DefinePlugin({
@@ -15,6 +16,9 @@ const basePlugins = [
     template: './src/index.html',
     inject: 'body',
     minify: false
+  }),
+  new SassLintPlugin({
+    glob: 'src/**/*.scss',
   })
 ];
 
