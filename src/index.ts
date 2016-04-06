a9eab575f7ebee59ae851fc689f3e4ca13160f96
ecozoic <johnnn.mitchell@gmail.com>
2016-04-06 17:29:38 -0400

    angular prod mode

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import { enableProdMode } from 'angular2/core';
 import { bootstrap } from 'angular2/platform/browser';
 import { App } from './components/App';
 
+declare const __PRODUCTION__: any;
+
+if (__PRODUCTION__) {
+  enableProdMode();
+}
 
 bootstrap(App);
