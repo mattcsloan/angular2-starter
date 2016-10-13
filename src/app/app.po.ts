a44269a91d950c17a47bec39f3f983ced0c0f55d
ecozoic <johnnn.mitchell@gmail.com>
2016-10-13 09:45:57 -0400

    move some typings around to avoid errors during webpack build

--- src/app/app.po.ts
+++ src/app/app.po.ts
@@ -1,4 +1,5 @@
 /// <reference path="../../node_modules/@types/protractor/index.d.ts" />
+/// <reference path="../../node_modules/@types/selenium-webdriver/index.d.ts" />
 
 export class AppPage {
   navigateTo() {
